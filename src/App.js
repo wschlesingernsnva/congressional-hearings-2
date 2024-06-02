cdfca5737302bf88ebb1c54ea449a47aec71377e
wschlesingernsnva <wschlesinger@newschoolva.org>
2024-06-01 23:54:00 -0400

    Set default chakra theme to dark mode

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import "./App.css";
 import Main from "./Main.js";
 
 import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./chakraTheme.js";
 
 export default function App() {
 	return (
-		<ChakraProvider>
+		<ChakraProvider theme={theme}>
 			<div className="App">
 				<Main />
 			</div>
