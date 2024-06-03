ea76597298597729e7115b2af85caa5101a196ae
wschlesingernsnva <wschlesinger@newschoolva.org>
2024-06-02 21:16:07 -0400

    Sort App.js imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import Main from "./Main.js";
-
 import { ChakraProvider } from "@chakra-ui/react";
+
 import theme from "./chakraTheme.js";
+
+import Main from "./Main.js";
 
 export default function App() {
 	return (
