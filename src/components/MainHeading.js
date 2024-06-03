1b21bc27359608563c85e36fd1345a79309678b3
wschlesingernsnva <wschlesinger@newschoolva.org>
2024-06-03 12:45:17 -0400

    Add level 3 MainHeading support

--- src/components/MainHeading.js
+++ src/components/MainHeading.js
@@ -1,23 +1,33 @@
-import { Center, Heading } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
 
-const headingSizes = ["4xl", "2xl"];
+const headingSizes = ["4xl", "2xl", "lg"];
 const bottomMargins = [
 	{ base: "45px", md: "50px" },
 	{ base: "35px", md: "40px" },
+	"0px",
 ];
 
 export default function MainHeading(props) {
+	let align;
+	let topMargin;
+	if (props.level <= 2) {
+		align = "center";
+		topMargin = "15px";
+	} else {
+		align = props.align;
+		topMargin = "0px";
+	}
+
 	return (
-		<Center>
-			<Heading
-				textAlign="center"
-				size={headingSizes[props.level - 1]}
-				mt="15px"
-				mb={bottomMargins[props.level - 1]}
-				color={props.color}
-			>
-				{props.text}
-			</Heading>
-		</Center>
+		<Heading
+			align={align}
+			textAlign={align}
+			size={headingSizes[props.level - 1]}
+			mt={topMargin}
+			mb={bottomMargins[props.level - 1]}
+			color={props.color}
+		>
+			{props.text}
+		</Heading>
 	);
 }
