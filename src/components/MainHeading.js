3107275048f81d62617c4ab85c037f82012e4b10
wschlesingernsnva <wschlesinger@newschoolva.org>
2024-06-03 07:08:41 -0400

    Change bottom margin of main heading for deeper levels

--- src/components/MainHeading.js
+++ src/components/MainHeading.js
@@ -1,6 +1,7 @@
 import { Center, Heading } from "@chakra-ui/react";
 
 const headingSizes = ["4xl", "2xl"];
+const bottomMargins = ["50px", "35px"];
 
 export default function MainHeading(props) {
 	return (
@@ -9,7 +10,7 @@ export default function MainHeading(props) {
 				textAlign="center"
 				size={headingSizes[props.level - 1]}
 				mt="15px"
-				mb="50px"
+				mb={bottomMargins[props.level - 1]}
 				color={props.color}
 			>
 				{props.text}
