0c6f4dfb5a866ae4d7f5ea5fe7dc5c7b5e270b92
wschlesingernsnva <wschlesinger@newschoolva.org>
2024-06-04 11:29:33 -0400

    Update intro

--- src/pages/1_Introduction.js
+++ src/pages/1_Introduction.js
@@ -26,107 +26,110 @@ export default function Introduction() {
 					gap={8}
 				>
 					<GridItem order={{ base: 2, md: 1 }} colSpan={{ base: 1, md: 2 }}>
-						<VStack
-							spacing={5}
-							align={{ base: "start", md: "end" }}
-							textAlign={{ base: "start", md: "end" }}
-						>
+						<VStack spacing={5} textAlign="justify">
 							<Text>
 								On July 16, 1945, at 5:29 AM, the first nuclear bomb exploded in
-								New Mexico. Codenamed "Trinity", this titanic fireball was the
-								capstone of the United States’ Manhattan Project, demonstrating
-								a functional design for a plutonium-based nuclear weapon. The
-								very same design (nicknamed the “Fat Man”) along with a slightly
-								different uranium-based design (nicknamed the “Little Boy”) was
-								used for a second time, less than three weeks later, when the
-								United States detonated them over Hiroshima and Nagasaki City in
-								Japan, bringing a rapid end to World War II. These two
-								explosions have been, to date, the only time nuclear weapons
-								were ever used in warfare.
+								New Mexico. Codenamed <Text as="i">Trinity</Text>, this titanic
+								fireball was the capstone of the United States' Manhattan
+								Project, demonstrating a functional design for a plutonium-based
+								nuclear weapon. The very same design (nicknamed the "Fat Man")
+								along with a slightly different uranium-based design (nicknamed
+								the "Little Boy") was used for a second time, less than three
+								weeks later, when the United States detonated them over
+								Hiroshima and Nagasaki City in Japan, bringing a rapid end to
+								World War II. These two explosions have been, to date, the only
+								time nuclear weapons were ever used in warfare.
 							</Text>
 							<Text>
-								Global politics was forever changed. With the capacity for a
-								nuclear country to wreak absolute nuclear annihilation on any
-								foe, nuclear weapons development became a defining focus of the
-								next 50 years of politics and military research. Before long,
-								atomic spy Klaus Fuchs leaked the basic “Fat Man” design to
+								Global politics were forever changed. With the capacity for a
+								country to wreak absolute nuclear annihilation on any foe,
+								nuclear weapons development became a defining focus of the next
+								fifty years of politics and military research. Before long,
+								atomic spy Klaus Fuchs leaked the basic design of the Fat Man to
 								Russia, and Russia detonated its first atomic bomb at a test
 								site in Kazakhstan on August 29th, 1949. With the Soviets now a
 								nuclear rival, the U.S. began to greatly escalate its nuclear
 								weapons development—and began nuclear testing, resulting in the
-								complete environmental destruction of a number of pacific
+								complete environmental destruction of a number of Pacific
 								islands.
 							</Text>
 							<Text>
-								But this isn't the whole story. As residents of certain parts of
-								the western United States might be all too aware, America’s
+								<Text as="b">But this isn't the whole story.</Text> America's
 								nuclear experimentation had severe toxic effects on large swaths
 								of the southwest. In fact, even Trinity, the very first nuclear
 								test, was conducted near enough to populations of thousands of
 								civilians that such a test would be absolutely prohibited by
-								later nuclear testing guidelines. No effort was made to warn or
-								evacuate those living under the nuclear fallout. This negligence
-								soon became a clear pattern in the U.S. Government’s behavior:
-								in the Pacific, not even to mention effects on local
-								populations, many American sailors and marines were directly
-								exposed to nuclear blasts and accompanying ionizing radiation
-								which induced severe health effects both then and later in life.
-								As the military’s obsessive, reckless behavior and refusal to
-								compensate those harmed by its testing spiraled into several
-								nation-wide scandals, these soldiers and civilians’ struggle for
-								recognition and compensation continued.
+								later nuclear testing guidelines. And even after the
+								implementation of those guidelines, no effort was made to warn
+								or evacuate those living under the nuclear fallout. This
+								negligence soon became a clear pattern in the U.S. government's
+								behavior: in the Pacific, many American sailors and marines were
+								directly exposed to nuclear blasts and accompanying ionizing
+								radiation which induced severe health effects both then and
+								later in life—not even to mention the tests' effects on local
+								populations. As the U.S. military's obsessive, reckless behavior
+								and refusal to compensate those harmed by its testing spiraled
+								into several nation-wide scandals, these soldiers and civilians'
+								struggle for recognition and compensation continued.
+							</Text>
+						</VStack>
+					</GridItem>
+					<GridItem order={3} colSpan={{ base: 1, md: 2 }}>
+						<MainHeading level={3} align="start" text="Operation Crossroads" />
+						<VStack spacing={5} textAlign="justify">
+							<Text>
+								The first postwar nuclear test had been planned before a single
+								nuclear weapon was ever built. In June 1945, a month before
+								Trinity, Vice Admiral E.L. Cochrane and Rear Admiral G.F. Hussey
+								proposed a test to determine the effects of high-explosive
+								ordinance on warships and merchant vessels. They proposed the
+								use of real, retired navy ships in a full-scale test to provide
+								better prediction quality over miniature simulations. The idea
+								was well-received by military leaders, and preparations began.
+								However, following the end of the war, Cochrane changed the
+								plan, instead proposing that they "must be prepared to undertake
+								broad-scale experiments with the atomic bomb to clear up its
+								major influence on naval warfare before we can at all consider
+								an extension of the underwater explosion work on the concept of
+								TNT or its associated explosives." Soon enough, a joint task
+								force was formed, and despite pushback from scientists,
+								diplomats, and the public,{" "}
+								<Text as="i">Operation Crossroads</Text> began on July 1, 1946
+								at Bikini Atoll in the Marshall Islands.
 							</Text>
 							<Text>
-								Background The first post-war nuclear test was planned before a
-								single nuclear weapon had ever been built. In June 1945, a month
-								before <Text as="i">Trinity</Text>, Vice Admiral E.L. Cochrane
-								and Rear Admiral G.F. Hussey proposed a test to determine the
-								effects of high-explosive ordinance on warships and merchant
-								vessels. They proposed the use of real retired navy ships in a
-								full-scale test to provide better prediction quality over scale
-								model demolitions. The idea was well-received, and preparations
-								began. Following the end of the war, however, Cochrane changed
-								the plan, proposing instead that they “must be prepared to
-								undertake broad-scale experiments with the atomic bomb to clear
-								up its major influence on naval warfare before we can at all
-								consider an extension of the underwater explosion work on the
-								concept of TNT or its associated explosives.” Soon enough, a
-								joint task force was formed, and despite pushback from
-								scientists, diplomats, and the public, “Operation Crossroads”
-								began on July 1, 1946 at the Bikini Atoll in the Marshall
-								Islands.
+								The tests were codenamed <Text as="i">Able</Text>and{" "}
+								<Text as="i">Baker</Text>, and both bombs had a yield of around
+								23 kilotons, comparable to the 21-kiloton Fat Man bomb dropped
+								on Nagasaki City. The stated purpose of the two tests was to
+								determine the effects of nuclear devices in the context of naval
+								combat, and as such, they were tested on a fleet of
+								decommissioned ships. <Text as="i">Able</Text> was to be dropped
+								from a plane and detonated, creating an airburst explosion above
+								the surface of the water, while <Text as="i">Baker</Text> would
+								detonate beneath the surface of the water.
 							</Text>
 							<Text>
-								The tests were codenamed “Able” and “Baker”, and both bombs had
-								a yield of around 23 Kilotons of TNT, comparable to the 21 KT
-								“Fat Man” bomb dropped at Nagasaki. The stated purpose of the
-								test was to determine the effects of nuclear devices in the
-								context of naval combat, and as such, they were tested on
-								“fleets” of decommissioned ships. Able was to be dropped from a
-								plane and detonated, creating an airburst explosion above the
-								surface of the water, while “Baker” would detonate beneath the
-								surface of the water.
-							</Text>
-							<Text>
-								However, difficulties immediately arose. “Able”, fired on July
-								1, missed its intended target by over 2000 feet, reducing the
-								usefulness of the test greatly. Following this setback, the
-								state of the warships was determined, and shot “Baker” proceeded
+								However, when <Text as="i">Able</Text> was fired on July 1, it
+								missed the intended target of the decommissioned ships by over
+								two-thousand feet, greatly reducing the usefulness of the test.
+								Despite this setback, the state of the warships was assessed,
+								data was collected, and shot <Text as="i">Baker</Text> proceeded
 								on July 25.
 							</Text>
 							<Text>
-								Despite the difficulties of shot “Able”, shot “Baker” was a
-								disaster. Though “Baker” produced some useful information, it
-								moreso produced a massive amount of unexpected nuclear
-								contamination and fallout, preventing any further tests with the
-								“fleet”, and making the water around Bikini Atoll so radioactive
-								that the island became completely uninhabitable for decades.
+								<Text as="i">Baker</Text> was a disaster, releasing a massive
+								amount of unexpected nuclear contamination and fallout. This
+								contamination prevented any further tests with the fleet used
+								for <Text as="i">Able</Text> and <Text as="i">Baker</Text> and
+								made the water around Bikini Atoll so radioactive that the
+								island became completely uninhabitable for decades.
 							</Text>
 							<Text>
-								Armed with these two warnings about the dangers of nuclear
-								testing, and continuous protestation from scientists and others,
-								the United States would go on to conduct at least 200 more tests
-								in the atmosphere or underwater.
+								Faced with these two warnings about the dangers of nuclear
+								testing and continuous protestation from scientists and others,
+								the United States would go on to conduct at least two-hundred
+								more tests in the atmosphere and underwater.
 							</Text>
 						</VStack>
 					</GridItem>
